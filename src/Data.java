05657657bac4446ec465829ca3b407e10a20a939
Kyle Williams <kjwkc3@mail.umsl.edu>
2016-05-07 17:42:36 -0500

    Data is now able to be read from file and converted

--- src/Data.java
+++ src/Data.java
@@ -1,10 +1,61 @@
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Kyle on 5/5/2016.
  */
 public class Data {
     // FileHandler
     // lines of data
+    FileHandler fh = null;
+    String[] fileData = {};
+    List<HashMap<Integer, Integer>> data = new ArrayList<>();
+    int numDecisionVariables = 0;
+    int totalData = 0;
 
-    // convert file lines into data
+    public Data(FileHandler fh) {
+        this.fh = fh;
+        fileData = fh.linesInFile;
+
+        // fileData[0] := first line in file
+        // 0/1 := first / seconds words in line
+        // +1 in order to also get classification
+        // -1 in order to NOT get first line of fileData
+        numDecisionVariables = Integer.valueOf(fh.getWordAt(fileData[0], 0)) + 1;
+        totalData = Integer.valueOf(fh.getWordAt(fileData[0], 1)) - 1;
+
+        createData();
+    }
+
+    public void createData() {
+        for (int i = 1; i <= totalData; i++) {
+            HashMap<Integer, Integer> tmpDataLine = new HashMap<>();
+            String curLine = fileData[i];
+
+            for (int j = 0; j < numDecisionVariables; j++) {
+                int dataValue = 0;
+                dataValue = Integer.valueOf(fh.getWordAt(curLine, j));
+
+                if (j == numDecisionVariables - 1)
+                    tmpDataLine.put(0, dataValue);
+                else
+                    tmpDataLine.put(j + 1, dataValue);  // j+1 so that we don't accidentally put it in index 0
+            }
+
+            data.add(tmpDataLine);
+        }
+    }
+
+    public void displayData() {
+        System.out.println("Num elements: " + data.size());
+
+        for (HashMap<Integer, Integer> h : data) {
+            System.out.println(h);
+        }
+    }
+
     // override arraylist to deep copy
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by kjwkc3 on 5/4/2016.
+ *  // path to file: E:\workspace\Perceptron\Resources\apascal_train.txt
  */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        String filename;
 
         // get file from user
+        System.out.println("Enter a filename or path");
+        System.out.print("> ");
+        filename = sc.next();
+
+        sc.close();
+
+        Data data = new Data(new FileHandler(filename));
+        data.displayData();
+
         // turn file into data
         // pass data to perceptron
         // train the perceptron
